14305ae31f6965ab236720d89cc9a7a286fa55b7
Abdullahi <abdullahiabdulwasiu1@gmail.com>
2022-09-27 20:36:45 +0100

    Add Spinner, Error & Sidebar Components

---
+++ frontend/src/Components/Ui/SideBar/SideBarData.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useCallback } from "react";
+import { useSelector, useDispatch } from "react-redux";
+//import {useQueryParam,  useNavigate } from "react-router-dom";
+
+import { fetchPostsCategory } from "../../../store/actions/postsCategory";
+import classes from "./Sidebar.module.css";
+
+const SidebarData = (props) => {
+    const dispatch = useDispatch();
+    //const navigate = useNavigate();
+    // let query = "?post_category=others";
+    //console.log("Query", query);
+
+    const categoryPost = useSelector((state) => {
+        return state.postsCategory.posts;
+    });
+    console.log("CatPost", categoryPost); //logging the data from store
+    const query = props.query;
+    useEffect(() => {
+        dispatch(fetchPostsCategory(query));
+    }, [query]);
+
+    return (
+        <>
+            {categoryPost.map((post) => (
+                <div key={post.slug} className={classes.Container}>
+                    <div className={classes.ImageContainer}>
+                        <img src={post.photo2} alt="financehub" />
+                    </div>
+                    <div className={classes.Content}>
+                        <h5 className={classes.Category}>{post.category}</h5>
+                        <h5 className={classes.Text}>{post.title}</h5>
+                    </div>
+                </div>
+            ))}
+        </>
+    );
+};
+export default SidebarData;
---
+++ frontend/src/Components/Ui/SideBar/Sidebar.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react"; 
+import { Tabs, Tab } from "@mui/material";
+//import LatestIcon from "@mui/icons-material/AccessibilitySharp";
+//import TrendingIcon from "@mui/icons-material/ArrowOutwardSharp";
+//import { Avatar } from "@mui/material";
+import SidebarData from "./SideBarData";
+
+
+const SideBar = (props) => {
+    
+    const [value, setValue] = useState(0);
+    const [categoryState, setCategoryState] = useState("?recent_posts=6");
+
+    let catagoryType = "?recent_posts=6"
+    const CategoryHandler = (e, newValue) => {
+        setValue(newValue);
+        
+        if (newValue == 0) {
+             setCategoryState("?recent_posts=6")
+            
+             console.log("State1", categoryState)
+        }
+        if (newValue == 1) {
+             setCategoryState("?post_category=sport")
+             console.log("state2", categoryState)
+        }
+        if (newValue == 2) {
+             setCategoryState("?popular_posts=20")
+            
+             console.log("state3", categoryState)
+        }
+       
+        
+        //console.log("Categorytype", cati);
+    };
+
+    
+    
+    return (
+      
+        <div>
+            <Tabs
+                textColor="inherit"
+                value={value}
+                onChange={CategoryHandler}
+                // onChange={(e, newValue) => setValue(newValue)}
+                indicatorColor="secondary"
+            >
+                {/*<Tab component={Link} to="/order" label="Business" /> */}
+
+                <Tab label="Latest" />
+                {/* <Avatar
+                style={{ color: "#000", background: "#fff", fontSize: "7px" }}
+            >
+                <TrendingIcon />
+            </Avatar>*/}
+                <Tab label="Trending" />
+
+                <Tab label="Hot" />
+            </Tabs>
+
+            <SidebarData  query={categoryState}/> {/*some props require value that is constanly changing eg values from input, ensure you manage a state here. if you set the value directly or depend on some other function to run and change it, it would not work cus once component load first and props grab values it would no change, even if the value later =another thing, this is how react works components rerender only when state change */}
+        </div>
+    );
+};
+
+export default SideBar;
